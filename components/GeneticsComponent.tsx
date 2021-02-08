eb5c9a30743760b770586b92fa7883c95dc2d94b
Nehal Udyavar <nehaludyavar@gmail.com>
2021-02-08 09:44:07 -0800

    playing around with toggling orbit controls

--- components/GeneticsComponent.tsx
+++ components/GeneticsComponent.tsx
@@ -2,20 +2,29 @@ import { Canvas } from "react-three-fiber";
 import { lazy, Suspense } from "react";
 import { OrbitControls } from "@react-three/drei";
 
+interface Genetics {
+  slide: number;
+}
+
 // Loading GLTF files in Next is a mess (can't be in pages), so here's a
 // commit that works
 // https://github.com/pmndrs/react-three-fiber/discussions/504
 const DNA = lazy(() => import("./Dna-5"));
 
-const GeneticsComponent = () => {
+const GeneticsComponent = ({ slide }: Genetics) => {
   return (
     <Canvas
       concurrent
-      camera={{ fov: 60, near: 0.1, far: 1000, position: [6, 6, 6] }}
+      camera={{
+        fov: 60,
+        near: 0.1,
+        far: 1000,
+        position: [6, 6, 6],
+      }}
     >
       {/* <cameraHelper /> */}
       <Suspense fallback={null}>
-        <DNA />
+        <DNA exploreMode={slide === 0 ? true : false} />
         <gridHelper args={[20, 40, "blue", "hotpink"]} />
         <axesHelper args={[10]} />
         <spotLight
@@ -27,7 +36,7 @@ const GeneticsComponent = () => {
           shadow-mapSize-height={2048}
           // castShadow
         />
-        <OrbitControls />
+        {slide === 0 ? <OrbitControls /> : null}
       </Suspense>
     </Canvas>
   );
--- pages/genetics/index.tsx
+++ pages/genetics/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import { useState } from "react";
 import styles from "./genetics.module.css";
 
 // Load r3f components like this to "solve" those can't import module 3js issues
@@ -8,15 +9,20 @@ const GeneticsComponent = dynamic(
 );
 
 const GeneticsPage = () => {
+  const [slide, setSlide] = useState(0);
+
   return (
     <div className={styles.container}>
       <div className={styles["interactive-container"]}>
         <div className={styles["interactive-container--inner"]}>
-          <GeneticsComponent />
+          <GeneticsComponent slide={slide} />
         </div>
       </div>
       <div className={styles["text-container"]}>
         Welcome to genetics interactive
+        <button onClick={() => setSlide((slide) => slide + 1)}>Next</button>
+        <button onClick={() => setSlide(0)}>Reset</button>
+        <p>{`Slide ${slide}`}</p>
       </div>
     </div>
   );
