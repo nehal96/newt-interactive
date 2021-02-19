bc28468481eefb450f758d6e7fa3a4806b72ce8a
Nehal Udyavar <nehaludyavar@gmail.com>
2021-02-18 21:41:38 -0800

    Paragraph component + tweaking spacing

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export { default as ArticleContainer } from "./ArticleContainer";
 export { default as Lede } from "./Lede";
 export { default as Navbar } from "./Navbar";
+export { default as Paragraph } from "./Paragraph";
 export { default as Title } from "./Title";
--- pages/genetics/index.tsx
+++ pages/genetics/index.tsx
@@ -1,6 +1,12 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
-import { ArticleContainer, Navbar, Title, Lede } from "../../components";
+import {
+  ArticleContainer,
+  Navbar,
+  Title,
+  Lede,
+  Paragraph,
+} from "../../components";
 import styles from "./genetics.module.css";
 
 // Load r3f components like this to "solve" those can't import module 3js issues
@@ -21,6 +27,21 @@ const GeneticsPage = () => {
           How a handful of scientists, spanning almost a century, discovered the
           structure of DNA. Gonna make this a bit longer to test
         </Lede>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam
+          quisque id diam vel. At tempor commodo ullamcorper a. Consectetur a
+          erat nam at lectus urna duis convallis convallis. Quis commodo odio
+          aenean sed. Porta nibh venenatis cras sed felis eget velit aliquet.
+          Senectus et netus et malesuada fames ac turpis.
+        </Paragraph>
+        <Paragraph>
+          Mauris pharetra et ultrices neque ornare. Commodo odio aenean sed
+          adipiscing diam. A erat nam at lectus. Adipiscing at in tellus integer
+          feugiat scelerisque varius morbi enim. Ornare arcu dui vivamus arcu
+          felis. Vel pretium lectus quam id leo in vitae turpis massa. Odio ut
+          enim blandit volutpat maecenas volutpat blandit.
+        </Paragraph>
         <div className={styles.container}>
           <div className={styles["interactive-container"]}>
             <div className={styles["interactive-container--inner"]}>
