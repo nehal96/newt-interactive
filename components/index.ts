8e49b30671b4e365628ff0359930ba3f11734fd9
Nehal Udyavar <nehaludyavar@gmail.com>
2021-05-01 00:52:18 -0700

    input fields to select magic variables

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 export { default as ArticleContainer } from "./ArticleContainer";
 export { default as Footer } from "./Footer";
+export { default as Input } from "./Input";
 export { default as Lede } from "./Lede";
 export { default as Navbar } from "./Navbar";
 export { default as Paragraph } from "./Paragraph";
--- pages/magic-squares/index.tsx
+++ pages/magic-squares/index.tsx
@@ -1,22 +1,56 @@
-import { ArticleContainer, Navbar, Title, Lede } from "../../components";
+import { useState } from "react";
+import { ArticleContainer, Input, Navbar, Title, Lede } from "../../components";
 import MagicSquare from "./MagicSquare";
+import styles from "./magic-squares.module.css";
+
+function generate3x3MagicSquare(a: number, b: number, c: number) {
+  return [
+    [c - b, c + (a + b), c - a],
+    [c - (a - b), c, c + (a - b)],
+    [c + a, c - (a + b), c + b],
+  ];
+}
 
 const MagicSquaresInteractive = () => {
+  const [inputValues, setInputValues] = useState({
+    a: 1,
+    b: 3,
+    c: 5,
+  });
+
+  const handleInputChange = (value, name) => {
+    const val = !value || value === "" ? "" : Number(value);
+
+    setInputValues({ ...inputValues, [name]: val });
+  };
+
+  const [squareValues, setSquareValues] = useState(
+    generate3x3MagicSquare(inputValues.a, inputValues.b, inputValues.c)
+  );
+
   return (
     <>
       <Navbar />
       <ArticleContainer>
         <Title>The Mystery of Magic Squares</Title>
         <Lede>An animated exploration of magic squares</Lede>
-        <MagicSquare
-          name="main"
-          values={[
-            [2, 9, 4],
-            [7, 5, 3],
-            [6, 1, 8],
-          ]}
-          withTotals={true}
-        />
+        <div className={styles["input-group"]}>
+          {Object.keys(inputValues).map((name) => (
+            <Input
+              name={name}
+              type="number"
+              value={inputValues[name]}
+              min={0}
+              onChange={(e) => handleInputChange(e.target.value, e.target.name)}
+              className={styles.input}
+              withPrepend
+              prependText={name}
+              key={name}
+            />
+          ))}
+        </div>
+
+        <MagicSquare name="main" values={squareValues} withTotals={true} />
       </ArticleContainer>
     </>
   );
