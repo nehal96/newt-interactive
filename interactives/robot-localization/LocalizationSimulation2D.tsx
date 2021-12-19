5e15dbae6c022a3c98475c8d570fbfda344b9d9d
Nehal Udyavar <nehaludyavar@gmail.com>
2021-12-19 00:36:52 -0800

    implement sense function

--- interactives/robot-localization/LocalizationSimulation2D.tsx
+++ interactives/robot-localization/LocalizationSimulation2D.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { initializeBeliefs } from "./helpers";
 import styles from "./LocalizationSimulation.module.css";
 import { SimulationGrid } from "./types";
@@ -12,50 +13,90 @@ const LocalizationSimlation2D = () => {
     [O, O, B, O, B],
     [O, B, O, B, O],
   ];
-  const beliefs = initializeBeliefs(grid);
-  console.log(beliefs);
-  const [startingRowIndex, startingColIndex] = [4, 2];
+  const [beliefs, setBeliefs] = useState(initializeBeliefs(grid));
+  const [currentPosition, setCurrentPosition] = useState({ row: 4, col: 2 });
+  const pHit = 3;
+  const pMiss = 1;
+
+  const sense = (color: string, pHit: number, pMiss: number) => {
+    const height = grid.length;
+    const width = grid[0].length;
+    let total = 0;
+    const newBeliefs = [];
+
+    for (let i = 0; i < height; i++) {
+      const newBeliefsRow = [];
+
+      for (let j = 0; j < width; j++) {
+        const isHit = grid[i][j] === color ? 1 : 0;
+        const newBelief = beliefs[i][j] * (isHit * pHit + (1 - isHit) * pMiss);
+        newBeliefsRow.push(newBelief);
+
+        total += newBelief;
+      }
+
+      newBeliefs.push(newBeliefsRow);
+    }
+
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        newBeliefs[i][j] = newBeliefs[i][j] / total;
+      }
+    }
+
+    setBeliefs(newBeliefs);
+  };
 
   return (
-    <table className={styles.grid}>
-      <tbody>
-        {grid.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((val, colIndex) => (
-              <td
-                key={`r${rowIndex + 1}c${colIndex + 1}`}
-                className={styles.cell}
-                style={{ backgroundColor: val }}
-              >
-                <svg
-                  height="100%"
-                  width="100%"
-                  style={{ display: "block", margin: 0 }}
+    <>
+      <table className={styles.grid}>
+        <tbody>
+          {grid.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((val, colIndex) => (
+                <td
+                  key={`r${rowIndex + 1}c${colIndex + 1}`}
+                  className={styles.cell}
+                  style={{ backgroundColor: val }}
                 >
-                  <circle
-                    cx="50%"
-                    cy="50%"
-                    r={beliefs[rowIndex][colIndex] * 36}
-                    fill="darkblue"
-                  />
-                  {rowIndex === startingRowIndex &&
-                  colIndex === startingColIndex ? (
-                    <text
-                      x="50%"
-                      y="50%"
-                      dominantBaseline="middle"
-                      textAnchor="middle"
-                    >
-                      Q
-                    </text>
-                  ) : null}
-                </svg>
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+                  <svg
+                    height="100%"
+                    width="100%"
+                    style={{ display: "block", margin: 0 }}
+                  >
+                    <circle
+                      cx="50%"
+                      cy="50%"
+                      r={beliefs[rowIndex][colIndex] * 36}
+                      fill="darkblue"
+                    />
+                    {rowIndex === currentPosition.row &&
+                    colIndex === currentPosition.col ? (
+                      <text
+                        x="50%"
+                        y="50%"
+                        dominantBaseline="middle"
+                        textAnchor="middle"
+                      >
+                        Q
+                      </text>
+                    ) : null}
+                  </svg>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button
+        onClick={() =>
+          sense(grid[currentPosition.row][currentPosition.col], pHit, pMiss)
+        }
+        style={{ width: 200, marginTop: "2rem", alignSelf: "center" }}
+      >
+        Sense
+      </button>
+    </>
   );
 };
 
