7fbef2a74399a5274ad683a266547e3fef4ff1ce
Nehal Udyavar <nehaludyavar@gmail.com>
2021-12-26 19:50:13 -0800

    rewrite title + lede with tailwind classes

--- components/Lede/index.tsx
+++ components/Lede/index.tsx
@@ -1,11 +1,13 @@
-import styles from "./Lede.module.css";
-
 interface Lede {
   children: React.ReactNode;
 }
 
 const Lede = ({ children }: Lede) => {
-  return <h2 className={styles.lede}>{children}</h2>;
+  return (
+    <h2 className="font-xl text-slate-500 font-light justify-self-center self-center text-center mb-8 px-2 max-w-4xl sm:mb-12 lg:text-2xl lg:mb-16 lg:px-16">
+      {children}
+    </h2>
+  );
 };
 
 export default Lede;
--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,11 +1,13 @@
-import styles from "./Title.module.css";
-
 interface Title {
   children: React.ReactNode;
 }
 
 const Title = ({ children }: Title) => {
-  return <h1 className={styles.title}>{children}</h1>;
+  return (
+    <h1 className="font-title text-5xl justify-self-center self-center text-center lg:text-6xl lg:mb-10">
+      {children}
+    </h1>
+  );
 };
 
 export default Title;
