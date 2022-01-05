839af62566cb32b2542131cbe1c6499606eebdc6
Nehal Udyavar <nehaludyavar@gmail.com>
2022-01-04 18:19:26 -0800

    rearrange blocks + smaller on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,39 +12,41 @@ export default function Home() {
       <Navbar />
       <main className="flex flex-col flex-auto h-full w-full max-w-5xl p-12">
         <h1 className="text-4xl font-bold text-slate-800 mb-8">Blocks</h1>
-        <div className="flex">
-          <Link href="/blocks/dna">
-            <a className="w-40 mr-12 flex-wrap bg-white rounded-xl shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg">
-              <div className="md:flex">
-                <Image
-                  src="/images/DNA3d.jpg"
-                  height={160}
-                  width={160}
-                  className="rounded-t-xl"
-                  alt="3D model of DNA"
-                />
-              </div>
-              <div className="p-5">
-                <h3 className="text-lg font-medium text-slate-800">
-                  3D Model of DNA
-                </h3>
-              </div>
-            </a>
-          </Link>
+        <div className="flex flex-wrap">
           <Link href="/blocks/robot-localization">
-            <a className="w-40 bg-white rounded-xl shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg">
-              <div className="md:flex">
+            <a className="w-28 sm:w-40 mr-6 sm:mr-12 bg-white rounded-xl shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg">
+              <div className="">
                 <Image
                   src="/images/robot.jpg"
                   height={160}
                   width={160}
+                  layout="responsive"
                   className="rounded-t-xl"
                   alt="cartoon robot"
                 />
               </div>
-              <div className="p-5">
-                <h3 className="text-lg font-medium text-slate-800">
+              <div className="p-3 sm:p-5">
+                <h3 className="sm:text-lg font-medium text-slate-800">
                   Simple Robot Localization
+                </h3>
+              </div>
+            </a>
+          </Link>
+          <Link href="/blocks/dna">
+            <a className="w-28 sm:w-40 flex-wrap bg-white rounded-xl shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg">
+              <div className="">
+                <Image
+                  src="/images/DNA3d.jpg"
+                  height={160}
+                  width={160}
+                  layout="responsive"
+                  className="rounded-t-xl"
+                  alt="3D model of DNA"
+                />
+              </div>
+              <div className="p-3 sm:p-5">
+                <h3 className="sm:text-lg font-medium text-slate-800">
+                  3D Model of DNA
                 </h3>
               </div>
             </a>
