9578bd04877e2c155b3114aa15d66bed1cf16d23
Nehal Udyavar <nehaludyavar@gmail.com>
2022-01-10 00:12:24 -0800

    twitter + fb meta (lol) properties

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,27 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.newtinteractive.com/" />
+        <meta property="og:title" content="Newt Interactive" />
+        <meta
+          property="og:description"
+          content="Interactive, educational explainers and playgrounds on topics in science, technology, engineering, and math"
+        />
+        <meta property="og:image" content="/logo-banner.jpg" />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.newtinteractive.com/"
+        />
+        <meta property="twitter:title" content="Newt Interactive" />
+        <meta
+          property="twitter:description"
+          content="Interactive, educational explainers and playgrounds on topics in science, technology, engineering, and math"
+        />
+        <meta property="twitter:image" content="/logo-banner.jpg" />
       </Head>
       <Component {...pageProps} />
     </>
