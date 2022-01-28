17dd7cba96a2ecad7bc7cf4edc071b49843fce05
Nehal Udyavar <nehaludyavar@gmail.com>
2022-01-27 23:58:10 -0800

    init test api route for color spaces

--- pages/blocks/color-spaces/index.tsx
+++ pages/blocks/color-spaces/index.tsx
@@ -1,7 +1,19 @@
 import Head from "next/head";
-import { ArticleContainer, Lede, Navbar, Title } from "../../../components";
+import Image from "next/image";
+import {
+  ArticleContainer,
+  Button,
+  Lede,
+  Navbar,
+  Title,
+} from "../../../components";
 
 const ColorSpacePage = () => {
+  const convertImg = async () => {
+    const res = await fetch("/api/blocks/color-spaces/test");
+    res.json().then((stuff) => console.log(stuff));
+  };
+
   return (
     <>
       <Head>
@@ -11,6 +23,17 @@ const ColorSpacePage = () => {
       <ArticleContainer>
         <Title>Color Spaces</Title>
         <Lede>RGB, HSV, images</Lede>
+        <div className="h-[500px] flex flex-col items-center">
+          <Image
+            src="/images/puppies.jpg"
+            width={600}
+            height={400}
+            layout="fixed"
+          />
+          <Button variant="primary" onClick={convertImg}>
+            Black and white
+          </Button>
+        </div>
       </ArticleContainer>
     </>
   );
