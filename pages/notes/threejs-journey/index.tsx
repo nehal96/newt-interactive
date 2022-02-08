d6073e65861c2519a8b63cdc2a7215d9733e80ee
Nehal Udyavar <nehaludyavar@gmail.com>
2022-02-08 00:55:05 -0800

    init notes page + three js journey page

---
+++ pages/notes/threejs-journey/index.tsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import { ArticleContainer, Lede, Navbar, Title } from "../../../components";
+
+const ThreeJSJourneyPage = () => {
+  return (
+    <>
+      <Head>
+        <title>ThreeJS Journey Notes / Newt Interactive</title>
+        <meta
+          name="description"
+          content="Interactive, educational explainers and playgrounds on topics in science, technology, engineering, and math"
+        />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.newtinteractive.com/blocks/robot-localization"
+        />
+        <meta property="twitter:creator" content="@nehaludyavar" />
+        <meta property="twitter:title" content="Newt Interactive" />
+        <meta
+          property="twitter:description"
+          content="Interactive, educational explainers and playgrounds on topics in science, technology, engineering, and math"
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.ibb.co/NpXN5rj/Meta-tag-image-1.png"
+        />
+      </Head>
+      <Navbar />
+      <ArticleContainer>
+        <Title>ThreeJS Journey Notes</Title>
+        <Lede>
+          Random stuff from Bruno Simon's course,{" "}
+          <a
+            href="https://threejs-journey.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-slate-600 hover:text-slate-700 underline underline-offset-1 decoration-slate-700"
+          >
+            ThreeJS Journey
+          </a>
+        </Lede>
+      </ArticleContainer>
+    </>
+  );
+};
+
+export default ThreeJSJourneyPage;
