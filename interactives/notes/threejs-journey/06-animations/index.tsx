c327ff16ea026c9d87b2d97be7bbf98c671d9cee
Nehal Udyavar <nehaludyavar@gmail.com>
2022-02-08 23:08:25 -0800

    init slides

--- interactives/notes/threejs-journey/06-animations/index.tsx
+++ interactives/notes/threejs-journey/06-animations/index.tsx
@@ -1,18 +1,37 @@
 import dynamic from "next/dynamic";
+import { useState } from "react";
 import {
   InteractiveContainer,
   InteractiveTutorialContainer,
+  Slides,
 } from "../../../../components";
+import { getSlides } from "./slides";
 
 const AnimationsCanvas = dynamic(() => import("./AnimationsCanvas"), {
   ssr: false,
 });
 
+const SLIDES = getSlides();
+
 const Animations = () => {
+  const [slide, setSlide] = useState(1);
+
+  const goToNextSlide = () => setSlide(slide + 1);
+  const goToPreviousSlide = () => setSlide(slide - 1);
+  const onReset = () => setSlide(1);
+
   return (
     <>
       <InteractiveTutorialContainer>
-        <InteractiveContainer className="w-[400px] h-[400px]">
+        <Slides
+          slides={SLIDES}
+          currentSlideNumber={slide}
+          onBack={goToPreviousSlide}
+          onNext={goToNextSlide}
+          onReset={onReset}
+          className="lg:w-1/2"
+        />
+        <InteractiveContainer className="lg:w-1/2 self-center w-[400px] h-[400px]">
           <AnimationsCanvas />
         </InteractiveContainer>
       </InteractiveTutorialContainer>
---
+++ interactives/notes/threejs-journey/06-animations/slides.tsx
@@ -0,0 +1,7 @@
+export function getSlides() {
+  return {
+    1: {
+      text: <>1</>,
+    },
+  };
+}
