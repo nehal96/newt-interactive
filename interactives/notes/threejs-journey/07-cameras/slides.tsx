747b210a714b6ddc9bed7534cb4af8026787284f
Nehal Udyavar <nehaludyavar@gmail.com>
2022-02-14 22:50:12 -0800

    slide 3

--- interactives/notes/threejs-journey/07-cameras/slides.tsx
+++ interactives/notes/threejs-journey/07-cameras/slides.tsx
@@ -1,3 +1,5 @@
+import { InlineCode } from "../../../../components";
+
 export function getSlides() {
   return {
     1: {
@@ -15,7 +17,8 @@ export function getSlides() {
       text: (
         <>
           <p>
-            The first camera we're going to look at is the Perspective Camera.
+            The first camera we're going to look at is the{" "}
+            <span className="font-medium">Perspective Camera</span>.
           </p>
           <p>
             It's the same one that's used previously above, and it's the most
@@ -26,6 +29,33 @@ export function getSlides() {
             image as seen by our eyes.
           </p>
           <p>Let's look at some of the parameters we use to set it up.</p>
+        </>
+      ),
+    },
+    3: {
+      text: (
+        <>
+          <ol className="list-outside list-decimal">
+            <li className="ml-6 mb-4">
+              <span className="font-medium">Field of view</span>: The vertical
+              amount of the scene that can be viewed, measured in degrees.
+              Default is <InlineCode variant="medium">50</InlineCode>.
+            </li>
+            <li className="ml-6 mb-4">
+              <span className="font-medium">Aspect ratio</span>: The ratio of
+              width to height of (usually) the canvas, but you can specify.
+            </li>
+            <li className="ml-6 mb-4">
+              <span className="font-medium">Near</span>: How close the camera
+              can see. Any object closer than the value won't be rendered.
+              Default is <InlineCode variant="medium">0.1</InlineCode>.
+            </li>
+            <li className="ml-6 mb-4">
+              <span className="font-medium">Far</span>: How far the camera can
+              see. Any object further than the value won't be rendered. Default
+              is <InlineCode variant="medium">2000</InlineCode>.
+            </li>
+          </ol>
         </>
       ),
     },
