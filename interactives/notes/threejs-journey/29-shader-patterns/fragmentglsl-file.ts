6a1ec49a2b3b7c037300135e1be8f0cd563f8c31
Nehal Udyavar <nehaludyavar@gmail.com>
2023-10-09 16:15:12 -0400

    init selecting a pattern

--- interactives/notes/threejs-journey/29-shader-patterns/fragmentglsl-file.ts
+++ interactives/notes/threejs-journey/29-shader-patterns/fragmentglsl-file.ts
@@ -1,6 +1,14 @@
-const fragmentGlslFile = () => {
-  return `void main() {
-  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+import { shaderPatterns } from "./shader-patterns";
+
+const fragmentGlslFile = (patternNumber: number) => {
+  const fragmentCode =
+    shaderPatterns[patternNumber]?.code ||
+    `gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);`;
+
+  return `varying vec2 vUv;
+
+void main() {
+  ${fragmentCode}
 }
 `;
 };
---
+++ interactives/notes/threejs-journey/29-shader-patterns/shader-patterns.ts
@@ -0,0 +1,10 @@
+export const shaderPatterns = {
+  1: {
+    code: `// pattern 1
+  gl_FragColor = vec4(vUv, 1.0, 1.0);`,
+  },
+  2: {
+    code: `// pattern 2
+  gl_FragColor = vec4(vUv, 0.0, 1.0);`,
+  },
+};
