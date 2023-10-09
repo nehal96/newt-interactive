668be935827b967fe420f16318d680532bc40544
Nehal Udyavar <nehaludyavar@gmail.com>
2023-10-09 15:14:21 -0400

    init shader patterns code sandbox

---
+++ interactives/notes/threejs-journey/29-shader-patterns/index.tsx
@@ -0,0 +1,47 @@
+import { Sandpack } from "@codesandbox/sandpack-react";
+import { atomDark } from "@codesandbox/sandpack-themes";
+import indexHtmlFile from "./indexhtml-file";
+import stylesCssFile from "./stylescss-file";
+import indexJsFile from "./indexjs-file";
+import vertexGlslFile from "./vertexglsl-file";
+import fragmentGlslFile from "./fragmentglsl-file";
+
+const ShaderPatternsCodeSandbox = () => {
+  const files = {
+    "/index.html": {
+      code: indexHtmlFile(),
+      hidden: true,
+    },
+    "/index.js": indexJsFile(`${vertexGlslFile()}`, fragmentGlslFile()),
+    "/styles.css": {
+      code: stylesCssFile(),
+      hidden: true,
+    },
+    "/vertex.glsl": vertexGlslFile(),
+    "/fragment.glsl": fragmentGlslFile(),
+  };
+
+  return (
+    <Sandpack
+      files={files}
+      theme={atomDark}
+      template="vanilla"
+      options={{
+        showLineNumbers: true,
+        editorHeight: 500,
+      }}
+      customSetup={{
+        dependencies: {
+          three: "0.151.3",
+        },
+        devDependencies: {
+          "@babel/core": "7.2.0",
+          "parcel-bundler": "^1.6.1",
+        },
+        entry: "index.html",
+      }}
+    />
+  );
+};
+
+export default ShaderPatternsCodeSandbox;
