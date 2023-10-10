b7bbf0edd86dc5acc59d884b32515604736632b1
Nehal Udyavar <nehaludyavar@gmail.com>
2023-10-10 01:38:13 -0400

    create menu for selecting patterns

--- interactives/notes/threejs-journey/29-shader-patterns/fragmentglsl-file.ts
+++ interactives/notes/threejs-journey/29-shader-patterns/fragmentglsl-file.ts
@@ -1,13 +1,14 @@
-import { shaderPatterns } from "./shader-patterns";
+import { ShaderPattern } from "./shader-patterns";
 
-const fragmentGlslFile = (patternNumber: number) => {
+const fragmentGlslFile = (shader: ShaderPattern) => {
+  const comment = `// ${shader.name}`;
   const fragmentCode =
-    shaderPatterns[patternNumber]?.code ||
-    `gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);`;
+    shader?.code || `gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);`;
 
   return `varying vec2 vUv;
 
 void main() {
+  ${comment}
   ${fragmentCode}
 }
 `;
