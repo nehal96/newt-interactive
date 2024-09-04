618d99c1251fb7467d550a906ea3b35f9a7e8596
Nehal Udyavar <nehaludyavar@gmail.com>
2024-09-04 16:24:25 -0700

    init block page

---
+++ pages/blocks/transcription-networks/index.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import {
+  ArticleContainer,
+  Lede,
+  Navbar,
+  Paragraph,
+  Title,
+  PostArticleSubscribe,
+} from "../../../components";
+
+const TranscriptionNetworkPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Transcription Networks / Newt Interactive</title>
+        <meta
+          name="description"
+          content="Interactive explainer on transcription networks and their role in gene regulation"
+        />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:creator" content="@nehaludyavar" />
+        <meta
+          property="twitter:url"
+          content="https://www.newtinteractive.com/"
+        />
+        <meta property="twitter:title" content="Transcription Networks" />
+        <meta
+          property="twitter:description"
+          content="Interactive explainer on transcription networks and their role in gene regulation"
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.ibb.co/placeholder-image-url.png"
+        />
+      </Head>
+      <Navbar />
+      <ArticleContainer>
+        <Title>Transcription Networks</Title>
+        <Lede>Understanding the complex interactions in gene regulation</Lede>
+        <Paragraph>
+          Transcription networks are complex systems that regulate gene
+          expression in cells. They consist of transcription factors (TFs) that
+          bind to DNA and control the transcription of genes.
+        </Paragraph>
+        <PostArticleSubscribe />
+      </ArticleContainer>
+    </>
+  );
+};
+
+export default TranscriptionNetworkPage;
