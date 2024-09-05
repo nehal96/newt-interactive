ec32a91478d093ef61aba16b8526536516fd94ee
Nehal Udyavar <nehaludyavar@gmail.com>
2024-09-05 16:44:04 -0700

    add slides top navbar

--- components/SlideDeck/hooks.ts
+++ components/SlideDeck/hooks.ts
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import _ from "lodash";
 
-export default function useSlides() {
+export default function useSlides(slides) {
   const [slideIndex, setSlideIndex] = useState(0);
 
   const goToPreviousSlide = () => {
@@ -11,12 +12,26 @@ export default function useSlides() {
     setSlideIndex(slideIndex + 1);
   };
 
-  const onReset = () => setSlideIndex(1);
+  const onReset = () => setSlideIndex(0);
+
+  const jumpToSectionMenu =
+    slides[0].section &&
+    _.chain(slides)
+      .map((slide) => slide.section)
+      .uniq()
+      .value();
+
+  const onJumpToSection = (section) => {
+    const sectionIndex = slides.indexOf(section);
+    setSlideIndex(sectionIndex);
+  };
 
   return {
     slideIndex,
     goToPreviousSlide,
     goToNextSlide,
     onReset,
+    jumpToSectionMenu,
+    onJumpToSection,
   };
 }
