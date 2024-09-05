b631ea4d383427d34771fd92cc87f37273da2988
Nehal Udyavar <nehaludyavar@gmail.com>
2024-09-04 23:29:03 -0700

    connect hill func vars to input sliders

--- interactives/transcription-networks/TranscriptionNetworkTutorial.tsx
+++ interactives/transcription-networks/TranscriptionNetworkTutorial.tsx
@@ -29,12 +29,18 @@ const getHillFunctionData = (
 };
 
 const TranscriptionNetworkTutorial = () => {
+  const [beta, setBeta] = useState(10);
+  const [K, setK] = useState(1);
+  const [n, setN] = useState(1);
+
+  const hillFunctionData = getHillFunctionData(beta, K, n, 0, 20);
+
   return (
     <NestedInteractiveContainer>
       <NestedInteractiveTextContainer>
-        <p className="mb-4">
+        <div>
           This graph shows the Hill function for an activator:
-          <div className="my-4">
+          <div className="my-4 flex items-center justify-center">
             <MathFormula tex="f(X^*) = \beta \frac{X^{*n}}{K^n + X^{*n}}" />
           </div>
           where:
@@ -48,7 +54,54 @@ const TranscriptionNetworkTutorial = () => {
           <MathFormula tex="X^*" /> is the concentration of transcription factor
           in its active form
           <br />
-        </p>
+        </div>
+        <div>
+          <div className="mt-4">
+            <label htmlFor="beta-slider" className="font-medium block">
+              <MathFormula tex="\beta" />: {beta}
+            </label>
+            <input
+              type="range"
+              id="beta-slider"
+              min="0"
+              max="20"
+              step="0.1"
+              value={beta}
+              onChange={(e) => setBeta(parseFloat(e.target.value))}
+              className="w-11/12 flex-auto cursor-pointer"
+            />
+          </div>
+          <div className="mt-4">
+            <label htmlFor="K-slider" className="font-medium block">
+              <MathFormula tex="K" />: {K}
+            </label>
+            <input
+              type="range"
+              id="K-slider"
+              min="0.1"
+              max="10"
+              step="0.1"
+              value={K}
+              onChange={(e) => setK(parseFloat(e.target.value))}
+              className="w-11/12 flex-auto cursor-pointer"
+            />
+          </div>
+          <div className="mt-4">
+            <label htmlFor="n-slider" className="font-medium block">
+              <MathFormula tex="n" />: {n}
+            </label>
+            <input
+              type="range"
+              id="n-slider"
+              min="0.1"
+              max="4"
+              step="0.1"
+              value={n}
+              onChange={(e) => setN(parseFloat(e.target.value))}
+              className="w-11/12 flex-auto cursor-pointer"
+            />
+          </div>
+        </div>
       </NestedInteractiveTextContainer>
       <NestedInteractiveCanvasContainer className="bg-white">
         <VictoryChart domain={{ x: [0, 20], y: [0, 20] }}>
@@ -57,7 +110,7 @@ const TranscriptionNetworkTutorial = () => {
             domain={[0, 20]}
             standalone={false}
             style={{
-              axis: { stroke: "#94a3b8" },
+              axis: { stroke: "#090a0b" },
               axisLabel: { padding: 30 },
               tickLabels: { fontSize: 14, fill: "#334155" },
               ticks: { stroke: "#94a3b8", size: 4 },
@@ -81,8 +134,8 @@ const TranscriptionNetworkTutorial = () => {
               data: { stroke: "#c43a31" },
               parent: { border: "1px solid #ccc" },
             }}
-            data={getHillFunctionData()}
-            interpolation="natural"
+            data={hillFunctionData}
+            interpolation="basis"
           />
         </VictoryChart>
       </NestedInteractiveCanvasContainer>
