d4e84e0f105bdd452f93a66d93489672edee5677
Nehal Udyavar <nehaludyavar@gmail.com>
2024-09-25 04:42:40 -0700

    update prose

--- pages/explainers/systems-biology/transcription-network-basics-1.tsx
+++ pages/explainers/systems-biology/transcription-network-basics-1.tsx
@@ -27,34 +27,37 @@ const TranscriptionNetworkBasicsPartOne = () => {
           systems biology
         </Lede>
         <Paragraph>
-          Let's say that in one of your cells, the membrane has been damaged. In
-          order to fix the membrane, your cell needs to produce repair proteins.
-          How might it do so? To answer this, let's set up a simple framework,
-          and then bit by bit, fill in the details.
+          Let's say that in one of your cells, the membrane that houses the cell
+          has been damaged. In order to fix it, your cell needs to produce
+          repair proteins. How might it do so?
         </Paragraph>
         <Paragraph>
-          First, in order for your cell to begin creating repair proteins, it
-          must know that your cell is damaged &mdash; it needs some kind of
-          signal. Once it receives that signal, it can begin producing the
-          repair proteins to fix the membrane. Easy enough. But there's another
-          step: after your cell has done its repairs, it needs to know when to
-          stop producing repair proteins. Protein production uses up valuable
-          resources, so your cell probably shouldn't waste it creating repair
-          proteins it doesn't need. One of the simplest ways this is achieved is
-          if the cell stops transmitting the repair signal, and as a result
-          production stops.
+          To answer this, let's first set up a simple framework, and then bit by
+          bit, fill in the details. First, in order for your cell to begin
+          creating repair proteins, it must know that your cell is damaged
+          &mdash; it needs some kind of signal. Then, once it receives that
+          signal, it can begin producing the repair proteins to fix the
+          membrane. Easy enough.
+        </Paragraph>
+        <Paragraph>
+          But there's another step: after your cell has completed its repairs,
+          it needs to know when to stop producing repair proteins. Protein
+          production uses up valuable resources, so your cell probably shouldn't
+          waste it creating repair proteins it doesn't need. Once the repairs
+          are done, the cell can stop transmitting the repair signal, and as a
+          result stop production.
         </Paragraph>
         <Paragraph>
           So, we have a very simple framework so far: a cell receives a signal,
           begins producing corresponding proteins; stops receiving the signal,
-          stops producing the proteins. Like a traffic light for protein
-          production.
+          stops producing those proteins. Essentially, like a traffic light for
+          protein production.
         </Paragraph>
         <Paragraph>
           Proteins are encoded in genes, so when we talk about protein
-          production, we're really talking about gene expression. The presence
-          of the signal promotes gene expression, and its absence stops it. We
-          can show this in a simple diagram:
+          production, we're really talking about gene expression. In our simple
+          model, the presence of a signal promotes gene expression, and its
+          absence stops it. We can show this in a simple diagram:
         </Paragraph>
         <div className="flex justify-center mt-4 mb-12">
           <div className="max-w-[400px] w-full mx-auto">
@@ -72,16 +75,17 @@ const TranscriptionNetworkBasicsPartOne = () => {
           stretch of DNA that encodes a protein, normally represented in text by
           a series of bases: ACTAGCC, for example. An enzyme, RNA polymerase,
           attaches itself to a binding site just preceding the gene, and moves
-          along the gene and uses the information to synthesize mRNA. This
-          process is called <strong>transcription</strong>. The mRNA is
-          transported to the ribosome, the cell's molecular factory, where it is
-          used to synthesize, amino acid by amino acid, the new protein. This
-          process is called <strong>translation</strong>. The newly minted
-          protein is then transported, either inside or outside the cell, where
-          it can begin its job.
+          along the gene and uses the information to synthesize messenger RNA,
+          or mRNA for short. This process is called{" "}
+          <strong>transcription</strong>. The mRNA is transported to the
+          ribosome, the cell's molecular factory, where it is used to synthesize
+          &mdash; amino acid by amino acid &mdash; the new protein. This process
+          is called <strong>translation</strong>. The newly minted protein is
+          then transported, either inside or outside the cell, where it can
+          begin its job.
         </Paragraph>
         <Paragraph>
-          So, an updated diagram would look something like this:
+          We can update our diagram to include some of these details:
         </Paragraph>
         <div className="flex justify-center mt-4 mb-12">
           <div className="max-w-[500px] w-full mx-auto">
@@ -95,27 +99,25 @@ const TranscriptionNetworkBasicsPartOne = () => {
           </div>
         </div>
         <Paragraph>
+          It still illustrates the same process as the very simplified one
+          above, but with the specific machinery that enable gene expression.
           There's a small part of the puzzle still missing: how the signal{" "}
-          <strong>S</strong> plays a role here. We began with the example of a
-          signal that represents membrane damage, but cells have many different
-          kinds of of signals that indicate a change in their environment, like
-          higher temperature, an influx of nutrients or toxins, damage to DNA
-          (or membranes), and even signaling molecules from other cells
-          (hormones are one example of this). To bridge this signal to a change
-          in protein production, cells use a special protein called a
-          transcription factor that acts as a representative of the cell's
-          environmental state. Transcription factors can shift rapidly between
-          an active and inactive state based on the presence (or lack thereof)
-          of a signal. When active, they bind to DNA and regulate the rate at
-          which genes are read, like a molecular valve. As the name suggests,
-          they are a factor in transcription.
+          <strong>S</strong> plays a role here. We know, from the simple
+          framework we set up above, that it should play the role of a switch:
+          turn on gene expression when the signal is present; turn off gene
+          expression when it's not.
         </Paragraph>
         <Paragraph>
-          Let’s update our diagram again (including one more additional piece of
-          information: the region that precedes the gene, where the binding
-          sites for RNAp and transcription factors lie, is called the{" "}
-          <strong>promoter</strong>):
+          The missing piece, the macromolecule that enables this switch-like
+          functionality, is a special protein known as a{" "}
+          <strong>transcription factor</strong>. Transcription factors can shift
+          rapidly between an active and inactive state based on the presence (or
+          lack thereof) of a signal. When active, they bind to DNA and regulate
+          the rate at which genes are read, like a molecular valve. In effect,
+          they play a very important role acting as a bridge between a signal
+          and a change in protein production.
         </Paragraph>
+        <Paragraph>Let’s update our diagram again:</Paragraph>
         <div className="flex justify-center mt-4 mb-12">
           <div className="max-w-[550px] w-full mx-auto">
             <Image
@@ -139,25 +141,45 @@ const TranscriptionNetworkBasicsPartOne = () => {
           </div>
         </div>
         <Paragraph>
-          So now: a signal activates a transcription factor, which binds to its
-          binding site, which enables RNA polymerase to bind to its binding site
-          and begin transcribing DNA into mRNA, which is translated into a
-          protein. This is just one simplified example of a sequence of steps,
-          but it ‘s important to understand the biology that we’ll subsequently
-          abstract away.
+          This is a simple model of how a cell responds to a signal. Our
+          example, membrane damage, is just one of the many signals cells can
+          send to communicate changes in their environment, including higher
+          temperature, an influx of nutrients or toxins, damage to DNA, and even
+          signaling molecules from other cells (hormones are one example of
+          this). All these signals interact with transcription factors.
         </Paragraph>
         <Paragraph>
           Transcription factors are also proteins, so they themselves are
           encoded in genes, which can be regulated by other transcription
           factors, which in turn can be regulated by other transcription
-          factors, and so on. This complex set of interactions can be modeled as
-          a network &mdash; transcription networks &mdash; and as we will
-          uncover in this series, result in some very interesting properties.
-          The analysis will uncover the incredible solutions life has found for
-          problems it faces: how it self-regulates, how it responds to different
-          situations, how it performs logical operations (like OR and AND gates
-          in computers), and how these phenomena give organisms an advantage in
-          surviving and replicating.
+          factors, and so on. This creates a pretty complex set of interactions,
+          and can be modeled as a network &mdash;{" "}
+          <strong>transcription networks</strong>. As we will uncover in this
+          series, combining transcription factors results in some very
+          interesting properties and incredible solutions life has found for
+          problems it faces. We’ll explore how cells can respond instantly or
+          with a delay when necessary; how they can perform logical operations,
+          their own version of OR and AND gates from computers; how different
+          network structures keep showing up in different forms of life and how
+          they give organisms an evolutionary advantage; how cells can create
+          memory, and much, much more.
+        </Paragraph>
+        <div className="flex justify-center mt-4 mb-12">
+          <Image
+            src="/images/transcription-network-systems-biology.png"
+            alt="Transcription Network in E. coli"
+            width={600}
+            height={600}
+          />
+        </div>
+        <Paragraph>
+          For now, I’ll leave you with this picture of a transcription network
+          that includes about 20% of the genes in E. coli. It’s pretty complex,
+          but we’ll break them into smaller networks and find understandable
+          patterns in the series. In Part Two, we’ll identify the two types of
+          transcription factors, mathematically model increases or decreases in
+          protein production, and take a closer look at what those arrows in the
+          network represent.
         </Paragraph>
         <PostArticleSubscribe />
       </ArticleContainer>
