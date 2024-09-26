3273450305e4dd7a238db4d406776f97aaaab398
Nehal Udyavar <nehaludyavar@gmail.com>
2024-09-26 01:08:01 -0700

    change route name to series

--- pages/series/systems-biology/index.tsx
+++ pages/series/systems-biology/index.tsx
@@ -6,7 +6,6 @@ import {
   Navbar,
   Paragraph,
   Title,
-  PostArticleSubscribe,
   OrderedList,
 } from "../../../components";
 
@@ -33,21 +32,13 @@ const SystemsBiologyExplainersPage = () => {
         </Paragraph>
         <OrderedList>
           <li>
-            <Link href="/explainers/systems-biology/transcription-network-basics-1">
+            <Link href="/series/systems-biology/transcription-network-basics-1">
               <a className="text-blue-600 hover:underline">
                 Transcription Network Basics: Part One
               </a>
             </Link>
           </li>
-          {/* <li>
-            <Link href="/explainers/systems-biology/transcription-networks-part-two">
-              <a className="text-blue-600 hover:underline">
-                Transcription Networks: Part Two
-              </a>
-            </Link>
-          </li> */}
         </OrderedList>
-        {/* <PostArticleSubscribe /> */}
       </ArticleContainer>
     </>
   );
