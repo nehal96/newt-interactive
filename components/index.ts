757c91306d57b3e6bcd1496410259b6f145170ba
Nehal Udyavar <nehaludyavar@gmail.com>
2024-10-03 13:58:24 -0700

    delete chart x and y axis components

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,6 @@
 export { default as ArticleContainer } from "./ArticleContainer";
 export { default as Button } from "./Button";
 export { Code, InlineCode } from "./Code";
-export { ChartXAxis, ChartYAxis } from "./Chart";
 export { default as Footer } from "./Footer";
 export { default as ImageSeries } from "./ImageSeries";
 export {
