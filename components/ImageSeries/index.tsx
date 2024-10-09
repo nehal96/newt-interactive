73dbaa3828ae10e4ae424ed6ca0419b2c624d1d4
Nehal Udyavar <nehaludyavar@gmail.com>
2024-10-09 16:14:43 -0700

    convert transcription networks part one to use mdx

--- components/ImageSeries/index.tsx
+++ components/ImageSeries/index.tsx
@@ -43,41 +43,46 @@ const ImageSeries: React.FC<ImageSeriesProps> = ({
           sizes="100vw"
           style={{
             width: "100%",
-            height: "auto"
-          }} />
+            height: "auto",
+          }}
+        />
       </div>
       {images[currentImage].caption && (
         <div className="max-w-[550px] w-full mx-auto text-center my-2 text-xs text-gray-500">
           {images[currentImage].caption}
         </div>
       )}
-      <div className="flex justify-center mt-4 mb-2">
-        {images.map((_, index) => (
-          <div
-            key={index}
-            className={`w-2 h-2 mx-1 rounded-full ${
-              index === currentImage ? "bg-black" : "bg-gray-300"
-            }`}
-          />
-        ))}
-      </div>
-      <div className="flex justify-center w-full max-w-[550px] mt-2">
-        <Button
-          variant="secondary"
-          onClick={handlePrev}
-          className="mr-4"
-          disabled={currentImage === 0}
-        >
-          <FiArrowLeft size={18} className="my-1" />
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={handleNext}
-          disabled={currentImage === images.length - 1}
-        >
-          <FiArrowRight size={18} className="my-1" />
-        </Button>
-      </div>
+      {images.length > 1 && (
+        <>
+          <div className="flex justify-center mt-4 mb-2">
+            {images.map((_, index) => (
+              <div
+                key={index}
+                className={`w-2 h-2 mx-1 rounded-full ${
+                  index === currentImage ? "bg-black" : "bg-gray-300"
+                }`}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center w-full max-w-[550px] mt-2">
+            <Button
+              variant="secondary"
+              onClick={handlePrev}
+              className="mr-4"
+              disabled={currentImage === 0}
+            >
+              <FiArrowLeft size={18} className="my-1" />
+            </Button>
+            <Button
+              variant="secondary"
+              onClick={handleNext}
+              disabled={currentImage === images.length - 1}
+            >
+              <FiArrowRight size={18} className="my-1" />
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
