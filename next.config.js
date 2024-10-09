33c2fef33e80c57355364ded87f957191f74b361
Nehal Udyavar <nehaludyavar@gmail.com>
2024-10-09 12:21:29 -0700

    init mdx

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 const withTM = require("next-transpile-modules")([
   "@codesandbox/sandpack-react",
 ]);
+const withMDX = require("@next/mdx")();
 
-module.exports = withTM({
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-});
+module.exports = withTM(
+  withMDX({
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  })
+);
