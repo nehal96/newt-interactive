5b1bde93a9cad446fba50ceed79165519c3c761a
Nehal Udyavar <nehaludyavar@gmail.com>
2024-10-19 17:31:25 +0800

    decay equation slide + popover

--- interactives/systems-biology/ProteinDecayResponseTimeTutorial.tsx
+++ interactives/systems-biology/ProteinDecayResponseTimeTutorial.tsx
@@ -4,8 +4,39 @@ import {
   VictoryContainer,
   VictoryLine,
 } from "victory";
-import { SlideDeck } from "../../components";
+import { MathFormula, Popover, SlideDeck } from "../../components";
 import { axisStyle } from "../../components";
+
+const ExponentialDecayEquationPopoverContent = () => (
+  <div className="flex flex-col text-md w-[350px] text-sm">
+    <p className="mb-3">starting with:</p>
+    <MathFormula tex="\dfrac{dY}{dt} = 0 - \alpha Y" />
+    <p className="mt-6 mb-3">we can rearrange to:</p>
+    <MathFormula tex="\dfrac{dY}{Y} = -\alpha \thinspace dt" />
+    <p className="mt-6 mb-3">integrating both sides:</p>
+    <MathFormula tex="\int \dfrac{dY}{Y} = \int -\alpha \thinspace dt" />
+    <MathFormula className="mt-3" tex="\ln |Y| = -\alpha t + C" />
+    <p className="mt-6 mb-3">
+      we can expontiate both sides to solve for <MathFormula tex="Y" />:
+    </p>
+    <MathFormula tex="e^{\ln |Y|} = e^{-\alpha t + C}" />
+    <MathFormula className="mt-3" tex="|Y| = e^{-\alpha t} \cdot e^C" />
+    <p className="mt-6 mb-3">
+      the constant <MathFormula tex="e^C" /> is the starting point of the decay,
+      known as the initial condition. Since this is describing the decay of{" "}
+      <MathFormula tex="Y" />, we can assume this to be the steady state
+      concentration <MathFormula tex="Y_{st}" />.
+    </p>
+    <p className="mb-4">
+      We can also remove the absolute value since the equation doesn't go below
+      zero, giving us:
+    </p>
+    <MathFormula
+      className="self-center outline outline-indigo-500 py-2 px-4"
+      tex="Y(t) = Y_{st} e^{-\alpha t}"
+    />
+  </div>
+);
 
 const ProteinDecayResponseTimeChart = () => {
   const noTicksStyle = {
@@ -61,7 +92,37 @@ export const ProteinDecayResponseTimeTutorial = () => {
     {
       text: (
         <>
-          <p>protein decay chart</p>
+          <p>
+            Starting with the equation{" "}
+            <MathFormula
+              className="text-lg"
+              tex="\dfrac{dY}{dt} = \beta - \alpha Y"
+            />{" "}
+            and setting <MathFormula className="text-lg" tex="\beta = 0" />,{" "}
+            <Popover
+              trigger={
+                <span className="underline decoration-indigo-500 decoration-2 underline-offset-[3px] hover:bg-indigo-100 cursor-pointer">
+                  we can work our way
+                </span>
+              }
+              content={<ExponentialDecayEquationPopoverContent />}
+              side="top"
+              triggerOnHover={true}
+            />{" "}
+            to the following equation:
+          </p>
+          <div className="flex flex-col justify-center mt-4 mb-12 mx-auto">
+            <MathFormula
+              className="text-lg"
+              tex="Y(t) = Y_{st} e^{-\alpha t}"
+            />
+          </div>
+          <p>
+            which describes the exponential decay of concentration{" "}
+            <MathFormula className="text-lg" tex="Y" /> over time, as shown in
+            the graph. <MathFormula className="text-lg" tex="Y_{st}" /> is the
+            steady state concentration.
+          </p>
         </>
       ),
       interactive: <ProteinDecayResponseTimeChart />,
